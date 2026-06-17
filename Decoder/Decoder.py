Fixed the training and backpropagation but still not commented  right

Decoder/Decoder.py
@@ -20,6 +20,9 @@ class Decoder(nn.Module):
             "add_output_1":None,
             "add_output_2":None,
             "add_output_3":None,
+            "norm_output_1":None,
+            "norm_output_2":None,
+            "norm_output_3":None,
             "mmha_output":None,
             "mha_output":None,
             "feed_forward_output":None,
@@ -34,17 +37,20 @@ class Decoder(nn.Module):
 
         add_output_1=mmha_output+input
 
+        self.cache["norm_output_1"]=norm_input_1
         self.cache["add_output_1"]=add_output_1
         self.cache["mmha_output"]=mmha_output
 
         print("waaasss")
         
         #Q from Decoder rest from Encoder
+        
         norm_input_2=self.norm2.apply_norm(add_output_1)
         mha_output=self.mha.calculate(norm_input_2,encoder_output,encoder_output)
 
         add_output_2=mha_output+add_output_1
-
+        
+        self.cache["norm_output_2"]=norm_input_2
         self.cache["add_output_2"]=add_output_2
         self.cache["mha_output"]=mha_output
 
@@ -53,6 +59,7 @@ class Decoder(nn.Module):
 
         add_output_3=feed_forward_output+add_output_2
 
+        self.cache["norm_output_3"]=norm_input_3
         self.cache["add_output_3"]=add_output_3
         self.cache["feed_forward_output"]=feed_forward_output
 

Main/Train.py
@@ -22,19 +22,18 @@ from Transformer import Transformer
 train_Transformer=Transformer()
 
 #load old transformer values
-#train_Transformer.load_state_dict(torch.load('trained_transformer.pth'))
-#train_Transformer.eval()
+train_Transformer.load_state_dict(torch.load(r"C:\Users\Janis\Downloads\Transformer\trained_transformer.pth"))
+
 
 input=[("hi")]
 
-for i in range(11):
+for i in range(2):
     input.append("hi")
 
 print(input)
-train_Transformer.train(input,0.01)
+train_Transformer.train(input,1)
 
 
 #dataset= load_dataset("wikipedia", "20220301.en", split="train",streaming=True)
 
-print("hi")
-torch.save(train_Transformer.state_dict(), r"C:\Users\Praktikant\Downloads\first_transformer\trained_Transformer.pth")
+torch.save(train_Transformer.state_dict(), r"C:\Users\Janis\Downloads\Transformer\trained_transformer.pth")

Main/Transformer.py
@@ -30,8 +30,8 @@ class Transformer(nn.Module):
         self.context_size=1024
         self.num_heads=8
         self.num_layers=6
-        self.batch_size=12
-        self.vokab_size=50259
+        self.batch_size=3
+        self.vokab_size=50260
 
         self.Encoder1=Encoder(self.d_model,self.context_size,self.num_heads, self.num_layers,self.batch_size)
         self.Encoder2=Encoder(self.d_model,self.context_size,self.num_heads, self.num_layers,self.batch_size)
@@ -57,30 +57,30 @@ class Transformer(nn.Module):
 
         Encoder_input=Encoder_Embeddings+Positional_Encoding.unsqueeze(0)
 
-        print(Encoder_input.shape)
+        
 
         for Encoder in self.Encoder_List:
             Encoder_input=Encoder.forward(Encoder_input)
-            print(Encoder_input.shape)
+            
 
         Encoder_output=Encoder_input
         Decoder_input=Encoder_output
 
-        print(Encoder_input.shape)
+        
 
         for Decoder in self.Decoder_List:
             Decoder_input=Decoder.forward(Decoder_input,Encoder_output)
 
-        print("lol")
+        
 
         output=self.LinearLayer1.forward(Decoder_input)
 
         output_probabilities = torch.softmax(output,dim=-1)
 
-        print("hi")
+        
 
-        #Encoder_Embeddings are the target
-        Backpropagation().calculate(self.Decoder_List, self.Encoder_List, self.LinearLayer1, output_probabilities, Encoder_Embeddings,Encoder_Ids,Decoder_Ids,Learningrate,self.context_size, self.batch_size)
+        #Encoder_Ids are the target
+        Backpropagation().calculate(self.Decoder_List, self.Encoder_List, self.LinearLayer1, output_probabilities, Encoder_Ids,Encoder_Ids,Decoder_Ids,Learningrate,self.context_size, self.batch_size)
 
 
 